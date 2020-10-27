8fc9236f6aa023f951a409ac1bebc21b3e56c4e9
Danches <danchik995@gmail.com>
2020-10-27 14:44:34 +0300

    выкладываем фото

---
+++ js/image.js
@@ -0,0 +1,35 @@
+
+
+'use strict';
+
+
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+const fileChooserAvatar = document.querySelector('#avatar');
+const previewAvatar = document.querySelector('.ad-form-header__preview img');
+const fileChooserHome = document.querySelector('#images');
+const previewHome = document.querySelector('.ad-form__photo img');
+const fileChooserHandler = function (fileChooser, preview) {
+  fileChooser.addEventListener('change', function () {
+    const file = fileChooser.files[0];
+    const fileName = file.name.toLowerCase();
+
+
+    const matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      const reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+}
+fileChooserHandler(fileChooserAvatar, previewAvatar);
+fileChooserHandler(fileChooserHome, previewHome);
+
