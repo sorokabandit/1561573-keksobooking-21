69d60b8c6b4d1a52ad229e7162ccc1c481016216
Danches <danchik995@gmail.com>
2020-10-27 12:44:24 +0300

    модель сборки

--- js/XMLHttpRequest.js
+++ js/XMLHttpRequest.js
@@ -1,59 +1,52 @@
 'use strict';
+const loadURL = `  https://21.javascript.pages.academy/keksobooking/data`;
+const saveURL = `https://21.javascript.pages.academy/keksobooking`;
+const StatusCode = {
+  OK: 200
+};
+const TIMEOUT_IN_MS = 10000;
+window.load = function (onLoad, onError) {
+  const loadxhr = new XMLHttpRequest();
+  loadxhr.responseType = `json`;
+  loadxhr.addEventListener(`load`, function () {
+    if (loadxhr.status === StatusCode.OK && onLoad) {
+      onLoad(loadxhr.response);
+    } else if (onError) {
+      onError(`Статус ответа: ` + loadxhr.status + ` ` + loadxhr.statusText);
+    }
+  });
+  loadxhr.addEventListener(`error`, function () {
+    if (onError) {
+      onError(`Произошла ошибка соединения`);
+    }
 
+  });
 
-  const loadURL = `  https://21.javascript.pages.academy/keksobooking/data`;
-  const saveURL = `https://21.javascript.pages.academy/keksobooking`;
-  const StatusCode = {
-    OK: 200
-  };
-  const TIMEOUT_IN_MS = 10000;
-  window.load = function (onLoad, onError) {
-    const loadxhr = new XMLHttpRequest();
-    loadxhr.responseType = `json`;
+  loadxhr.addEventListener(`timeout`, function () {
+    if (onError) {
+      onError(`Запрос не успел выполниться за ` + loadxhr.timeout + `мс`);
+    }
+  });
 
+  loadxhr.timeout = TIMEOUT_IN_MS;
 
-    loadxhr.addEventListener(`load`, function () {
-      if (loadxhr.status === StatusCode.OK && onLoad) {
-        onLoad(loadxhr.response);
-      } else if (onError) {
-        onError(`Статус ответа: ` + loadxhr.status + ` ` + loadxhr.statusText);
-      }
-    });
-    loadxhr.addEventListener(`error`, function () {
-      if (onError) {
-        onError(`Произошла ошибка соединения`);
-      }
+  loadxhr.open(`GET`, loadURL);
+  loadxhr.send();
+};
+window.save = function (data, onLoad, onError) {
+  const savexhr = new XMLHttpRequest();
+  savexhr.responseType = `json`;
 
-    });
-
-    loadxhr.addEventListener(`timeout`, function () {
-      if (onError) {
-        onError(`Запрос не успел выполниться за ` + loadxhr.timeout + `мс`);
-      }
-
-    });
-
-    loadxhr.timeout = TIMEOUT_IN_MS;
-
-    loadxhr.open(`GET`, loadURL);
-    loadxhr.send();
-  };
-
-
-  window.save = function (data, onLoad, onError) {
-    const savexhr = new XMLHttpRequest();
-    savexhr.responseType = `json`;
-
-    savexhr.addEventListener(`load`, function () {
-      onLoad(savexhr.response);
-    });
-    savexhr.addEventListener(`error`, function () {
-      if (onError) {
-        onError(`Произошла ошибка соединения`);
-      }
-    });
-    savexhr.open(`POST`, saveURL);
-    savexhr.send(data);
-  };
+  savexhr.addEventListener(`load`, function () {
+    onLoad(savexhr.response);
+  });
+  savexhr.addEventListener(`error`, function () {
+    if (onError) {
+      onError(`Произошла ошибка соединения`);
+    }
+  });
+  savexhr.open(`POST`, saveURL);
+  savexhr.send(data);
+};
 
 
--- js/debounce.js
+++ js/debounce.js
@@ -1,19 +1,19 @@
 'use strict';
 
 
-  const DEBOUNCE_INTERVAL = 500; // ms
+const DEBOUNCE_INTERVAL = 500; // ms
 
-  const debounce = function (cb) {
-    let lastTimeout = null;
-    return function (...args) {
-      const parameters = args;
-      if (lastTimeout) {
-        window.clearTimeout(lastTimeout);
-      }
-      lastTimeout = setTimeout(function () {
-        cb(...parameters);
-      }, DEBOUNCE_INTERVAL);
-    };
+const debounce = function (cb) {
+  let lastTimeout = null;
+  return function (...args) {
+    const parameters = args;
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = setTimeout(function () {
+      cb(...parameters);
+    }, DEBOUNCE_INTERVAL);
   };
+};
 
-  window.debounce = debounce;
+window.debounce = debounce;
