c3dd296b6ceeec501c6869b74191c64242db9db4
Danches <danchik995@gmail.com>
2020-10-28 11:22:42 +0300

    fixed

--- js/data.js
+++ js/data.js
@@ -1,33 +1,8 @@
 'use strict';
 
 const buttonPin = document.querySelector('#pin').content.querySelector(`.map__pin`);
-const type = [`palace`, `flat`, `house`, `bungalow`];
-const checkin = [`12:00`, `13:00`, `14:00`];
-const features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 window.announcements = [];
 
-
-for (let i = 0; i < 8; i++) {
-  let obj = {};
-  obj.author = {};
-  obj.author.avatar = `img/avatars/user0${Math.floor(Math.random() * 7 + 1)}.png`;
-  obj.offer = {};
-  obj.offer.title = 'For sale';
-  obj.offer.address = `${Math.floor(Math.random() * 1000)}, ${Math.floor(Math.random() * 1000)}`;
-  obj.offer.price = Math.floor(Math.random() * 10000) + `₽/ночь`;
-  obj.offer.type = type[Math.floor(Math.random() * 4)];
-  obj.offer.rooms = Math.floor(Math.random() * 6);
-  obj.offer.guests = Math.floor(Math.random() * 10);
-  obj.offer.checkin = checkin[Math.floor(Math.random() * 2)];
-  obj.offer.checkout = checkin[Math.floor(Math.random() * 2)];
-  obj.offer.features = features[Math.floor(Math.random() * 5)] + ', ' + features[Math.floor(Math.random() * 5)];
-  obj.offer.description = 'огромная квартира для большой компании';
-  obj.offer.photos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-  obj.location = {};
-  obj.location.x = Math.floor(Math.random() * document.querySelector('.map__pins').getBoundingClientRect().width);
-  obj.location.y = Math.floor(Math.random() * 500 + 130);
-  window.announcements.push(obj);
-}
 const renderPins = (pins, limit) => {
   document.querySelectorAll('button.map__pin').forEach((pin) => {
     if (!pin.classList.contains('map__pin--main')) {
